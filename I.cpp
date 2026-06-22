a86c5cd561bfb67f45f1b8cebd139b08e10f4da3
Iftekhar Uddin Akil <iftekharuddinakil8@gmail.com>
2026-06-22 23:26:52 +0600

    Initial commit

---
+++ I.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0 && arr[i] >= arr[s - 1])
+        {
+            sum++;
+        }
+    }
+    cout << sum;
+
+    return 0;
+}
